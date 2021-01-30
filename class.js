a5347de49c2a19fd733a2bc06b2ffaeeeee44d3f
Mushbird <mushbird@kakao.com>
2021-01-30 18:48:04 +0900

    [Lecture6] Lecture End Commit

--- class.js
+++ class.js
@@ -48,5 +48,73 @@ class User {
 //console.log(user1.age);
 
 // 3. Fields (public, private)
+// Too soon!
+class Experiment {
+    publicFeild = 2;
+    #privateField = 0; // 최신 코드 (아직 쓰기에는 무리)
+}
+//const experiment = new Experiment();
+//console.log(experiment.publicFeild);
+//console.log(experiment.privateField);
+
+// 4. Static properties and methods
+// Too soon!
+class Article {
+    static publisher = 'Dream Coding';
+    constructor(articleNumber) {
+        this.articleNumber = articleNumber;
+    }
+
+    static printPublisher() {
+        console.log(Article.publisher);
+    }
+}
+
+//const article1 = new Article();
+//const article2 = new Article();
+//console.log(article1.publisher);
+
+// 5. Inheritance
+// a way for one class to extend another class.
+class Shape {
+    constructor(width, height, color) {
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+
+    draw() {
+        console.log('drawing ${this.color} color of');
+    }
+
+    getArea() {
+        return width * this.height;
+    }
+}
+
+class Rectangle extends Shape {}
+class Triangle extends Shape {
+    draw() {
+        super.draw();
+        console.log('so');
+    }
+    getArea() {
+        return (this.width * this.height) / 2;
+    }
+}
+
+const rectangle = new Rectangle(20, 20, 'blue');
+//rectangle.draw();
+//console.log(rectangle.getArea());
+const triangle = new Triangle(20, 20, 'red');
+//triangle.draw();
+//console.log(triangle.getArea()); 
+
+// 6. Class checking : istanceOf
+console.log(rectangle instanceof Rectangle);    // true
+console.log(triangle instanceof Rectangle);     // false
+console.log(triangle instanceof Triangle);      // true;
+console.log(triangle instanceof Shape);         // true;
+console.log(triangle instanceof Object);        // true; 
 
 
