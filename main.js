fd2a7c25bd2a910ac1f29486d51aa87795e91568
mushbird <0109bsh@gmail.com>
2021-02-02 23:06:32 +0900

    [Lecture2] Lecture End Commit

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 // 1. hello world!
 // live Server 설치 -> 실시간 스크립트 결과 확인!
+// flexible === dangerous(위험한 행동)
+// added ECMAScript 5
+
+// 바닐라자바스크립트로 개발할 때 상식적인 범위에서 사용할 수 있도록 
+// 그리고 성능개선, 변수 선언되지 않은 것을 사용방지
+'use strict'; 
 
 console.log('hello world!');
