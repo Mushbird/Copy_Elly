dc8e9e4f39b80fe820e4ac8fe90c3ff1068aa81c
mushbird <0109bsh@gmail.com>
2021-02-07 00:23:26 +0900

    [Lecture3] Intermediate Commit

--- variable.js
+++ variable.js
@@ -38,9 +38,9 @@ const maxNumber = 5;
 // object, box container
 // funcion, first-class funtion
 
-const count = 17; // integer
-const size = 17.1; // decimal number
-console.log(`value: ${const}, type: ${typeod count}`);
+const count = 17;   // integer
+const size = 17.1;  // decimal number
+console.log(`value: ${count}, type: ${typeof count}`);
 console.log(`value: ${size}, type: ${typeof size}`);
 
 // number - speicla numeric values: infinity, -infinity
@@ -63,7 +63,7 @@ const brendan = 'brendan';
 const greeting = 'hello ' + brendan;
 console.log(`value: ${greeting}, type: ${typeof greeting}`);
 const helloBob = `hi ${brendan}!`; // template literals (string)
-console.log(`value: ${helloBob}, type: ${typeof helloBob}`);
+console.log(`value: ${helloBob}, type: ${typeof helloBob}`);    // backtick을 이용하여 간편하게 + 기호를 생략사용
 console.log('value: ' + helloBob + ' type: ' + typeof helloBob);
 
 // boolean
@@ -74,5 +74,34 @@ const test = 3 < 1; // false
 console.log(`value: ${canRead}, type: ${typeof canRead}`);
 console.log(`value: ${test}, type: ${typeof test}`);
 
+// null
+let nothing = null;
+console.log(`value: ${nothing}, type: ${typeof nothing}`);
 
+// undefined
+let x;  // let x = undefined;
+console.log(`value: ${x}, type: ${typeof x}`);
+
+// symbol, create unique identifiers for objects
+const symbol1 = Symbol('id');
+const symbol2 = Symbol('id');
+console.log(symbol1 === symbol2);   // false
+const gSymbol1 = Symbol.for('id');
+const gSymbol2 = Symbol.for('id');
+console.log(gSymbol1 === gSymbol2); // true
+console.log(`value: ${symbol1.description}, type: ${typeof symbol1}`)
+
+// obect
+const ellie = {name: 'ellie', age: 20};
+ellie.age = 21; // 포인터로 찾아가서 값을 변경함
+
+// 5. Dynamic typing: dynamically typed language (타입 선언하지 않아도 값에 따라 변경처리)
+let text = 'hello';
+console.log(`value: ${text}, type: ${typeof text}`);
+text = 1;
+console.log(`value: ${text}, type: ${typeof text}`);
+text = '7' + 5;
+console.log(`value: ${text}, type: ${typeof text}`);
+text = '8' / '2';
+console.log(`value: ${text}, type: ${typeof text}`);
 
