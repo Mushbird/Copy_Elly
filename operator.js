f545e7074fd43a64d185d855711165666ba88ffb
mushbird <0109bsh@gmail.com>
2021-02-09 23:03:03 +0900

    [Lecture4] Intermediate Commit

--- operator.js
+++ operator.js
@@ -75,6 +75,28 @@ const stringFive = '5';
 const numberFive = 5;
 
 // == loose equality, with type conversion
-console.log(StringFive) 
+console.log(stringFive == numberFive);
+console.log(stringFive != numberFive);
 
-// 12:38부터
+// === strict equality, no type conversion
+console.log(stringFive === numberFive);
+console.log(stringFive !== numberFive);
+
+// object equality by reference
+const ellie1 = { name: 'ellie' };
+const ellie2 = { name: 'ellie' };
+const ellie3 = ellie1;
+console.log(ellie1 == ellie2);
+console.log(ellie1 === ellie2);
+console.log(ellie1 === ellie3);
+
+// equality - puzzler
+console.log(0 == false);
+console.log(0 === false);
+console.log('' == false);
+console.log('' === false);
+console.log(null == undefined);
+console.log(null === undefined);
+
+// 8. Conditional operators: if
+// if, else if, else
