58d469473c950b705889339b0c2d7225ad35a2ea
mushbird <0109bsh@gmail.com>
2021-02-12 21:35:11 +0900

    [Lecture4] Intermediate Commit

--- operator.js
+++ operator.js
@@ -160,9 +160,24 @@ for (let i = 3; i > 0; i = i - 2) {
 // nested loops
 for (let i = 0; i < 10; i++) {
     for (let j = 0; j < 10; j++) {
-        console.log(`i: ${i}, j:${j}`);
+        console.log(`i: ${i}, j: ${j}`);
     }
 }
 
 // break, continue
-// Q1. iterate from 0 to and print only even numbers
+// Q1. iterate from 0 to 10 and print only even numbers
+// (use continue)
+for (let i = 0; i < 11; i++) {
+    if (i % 2 === 0) {
+        console.log(`q1. ${i}`);
+    }
+}
+
+// Q2. iterate from 0 to 10 and print numbers until
+// reaching 8 (use break)
+for (let i = 0; i < 11; i++) {
+    if (i > 8) {
+        break;
+    }
+    console.log(`q2. ${i}`);
+}
