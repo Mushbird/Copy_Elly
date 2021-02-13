cd62dcad4fd931eecc1af6ade820bd6f40ae455e
mushbird <0109bsh@gmail.com>
2021-02-13 23:40:40 +0900

    [Lecture5] Intermediate Commit

--- funtion.js
+++ funtion.js
@@ -19,3 +19,30 @@ function log(message) {
 }
 log('Hello@');
 log(1234);
+
+// 2. Parameters
+// premitive parameters: passed by value
+// object parameters: passed by reference
+function changeName(obj) {
+    obj.name = 'coder'
+}
+const ellie = { name: 'ellie' };
+changeName(ellie);
+console.log(ellie);
+
+// 3. Default parameters (added in ES6)
+function showMessage(message, from = 'unknown') {
+    console.log(`${message} by ${from}`);
+}
+showMessage('Hi!');
+
+// 4. Rest parameters (added in ES6)
+function printAll(...args) {
+    for (let i = 0; i < args.length; i++) {
+            console.log(args[i]);
+    }
+    for (const arg of args) {
+        console.log(arg);
+    }
+}
+printAll('dream', 'coding', 'ellie');
