33d6934ee5eed0c18f2c485f14db97ff15d25d99
mushbird <0109bsh@gmail.com>
2021-02-17 22:44:37 +0900

    [Lecture7] Intermediate Commit

---
+++ objects.js
@@ -0,0 +1,31 @@
+// Objects
+// one of the JavaScript`s data types.
+// a collection of related data and/or functionality
+// Nearly all objects in JavaScript are instances of Object
+// object = { key : value };
+
+// const name = 'ellie';
+// const age = 4;
+// print(name, age);
+// function print(name, age) {
+//     console.log(name);
+//     console.log(age);
+// }
+
+// const ellie = { name: 'ellie', age: 4 };
+
+const obj1 = {};    // 'object literal' syntax
+const obj2 = new Object();  // 'object constructor' syntax
+
+function print(person) {
+    console.log(person.name);
+    console.log(person.age);
+}
+
+const ellie = { name:'ellie', age:'4' };
+print(ellie);
+
+// with JavaScript magic (dynamically typed language)
+// can add properties later
+ellie.hasJob = true;
+console.log(ellie.hasJob);
