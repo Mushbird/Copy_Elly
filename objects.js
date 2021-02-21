14af06a010e1f7b31d712cbd029b78c14c20d1f8
mushbird <0109bsh@gmail.com>
2021-02-21 09:55:44 +0900

    [Lecture7] Lecture End Commit

--- objects.js
+++ objects.js
@@ -14,6 +14,7 @@
 
 // const ellie = { name: 'ellie', age: 4 };
 
+
 // 1. Literals and properties
 const obj1 = {};    // 'object literal' syntax
 const obj2 = new Object();  // 'object constructor' syntax
@@ -23,18 +24,23 @@ function print(person) {
     console.log(person.age);
 }
 
-const ellie = { name:'ellie', age:'4' };
+const ellie = { name: 'ellie', age: 4 };
 print(ellie);
 
 // with JavaScript magic (dynamically typed language)
 // can add properties later
-delete ellie.hasJob = true;
+ellie.hasJob = true; // 뒤 늦게 추가한 것도 가능(가능한 자제)
 console.log(ellie.hasJob);
+
+delete ellie.hasJob;    // 삭제
+
 
 // 2. Computed properties
 // key should be always string
 console.log(ellie.name);
 console.log(ellie['name']);
+ellie['hasJob'] = true;
+console.log(eliie.hasJob);
 
 function printValue(obj, key) {
     console.log(obj[key]);
@@ -42,7 +48,70 @@ function printValue(obj, key) {
 printValue(ellie, 'name');
 printValue(ellie, 'age');
 
+
 // 3. Property value shorthand
 const person1 = { name: 'bob', age: 2};
 const person2 = { name: 'steve', age: 3};
 const person3 = { name: 'dave', age: 4};
+const person4 = new Person('ellie', 30)
+console.log(person4);
+
+
+// 4. Constructor function
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+
+// 5. in operator: property existence check (key in obj) Object 값 확인
+console.log('name' in ellie);
+console.log('age' in ellie);
+console.log('random' in ellie);
+console.log(ellie.random);
+
+
+// 6. for..in vs for..of
+// for (key in obj)
+console.clear();    // console.log 제거
+for (key in ellie) {
+    console.log(key);
+}
+
+// for (value of iterable)
+const array = [1, 2, 4, 5];
+// 기본
+for(let  i = 0; i < array.length; i++) {
+    console.log(array[i]);
+}
+
+// 간단하게
+for(value of array) {
+    console.log(value);
+}
+
+
+// 7. Fun cloning
+// Object.assign(dest, [obj1, obj2, obj3...])
+const user = { name:'ellie', age:20 };
+const user2 = user;
+user2.name = 'coder';
+console.log(user);
+
+// old way
+const user3 = {};
+for (key in user) {
+    user3[key] =  user[key];
+}
+console.clear();
+console.log(user3);
+
+const user4 = Object.assign({}, user);
+console.log(user4);
+
+// anoher example
+const fruit1 = { color: 'red' };
+const fruit2 = { color: 'blue', size: 'big' };
+const mixed = Object.assign({}. fruit1, fruit2); // 앞서 선언한 변수에 덮여씌움
+console.log(mixed.color);
+console.log(mixed.size);
