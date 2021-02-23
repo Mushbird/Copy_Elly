05c55c65a00291d9e0a7a4210b3d0bb7fc9be083
mushbird <0109bsh@gmail.com>
2021-02-23 23:27:08 +0900

    [Lecture8] Intermediate Commit

--- array.js
+++ array.js
@@ -16,4 +16,17 @@ console.log(fruits[1]);
 console.log(fruits[2]);
 console.log(fruits[fruits.length - 1]);
 
-// 3. 
+// 3. Looping over an array
+// print all fruits
+// a. for
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+}
+
+// b. for of
+for (let fruit of fruits) {
+    console.log(fruit);
+}
+
+// c. forEach
+fruits.forEach();
