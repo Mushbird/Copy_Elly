1ac7f6b19a5abd52db8cff2309aedae823146851
mushbird <0109bsh@gmail.com>
2021-02-26 23:31:53 +0900

    [Lecture9] Intermediate Commit

---
+++ array-api.js
@@ -0,0 +1,25 @@
+// Q1. make a String out of an array
+{
+    const fruits = ['apple', 'banana', 'orange'];
+    const result = fruits.join();
+    console.log(result);
+}
+
+// Q2. make an array out of a string
+{
+    const fruits = 'apple, kiwi, banana, cherry';
+    const result = fruits.split(",");
+    console.log(result);
+
+}
+
+// Q3. make this array look like this: [5, 4, 3, 2, 1]
+{
+    const array = [1, 2, 3, 4, 5];
+}
+
+// Q4. make new array without the first two elements
+{
+    const array = [1, 2, 3, 4, 5];
+}
+
