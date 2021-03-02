6175213124a36e234738db1ba6bf672d6b53481e
mushbird <0109bsh@gmail.com>
2021-03-03 00:23:42 +0900

    [Lecture10] Intermediate Commit

--- json.js
+++ json.js
@@ -30,6 +30,20 @@ json= JSON.stringify(rabbit, (key, value) => {
     console.log(`key: ${key}, value: ${value}`);
     return key === 'name' ? 'ellie' : value;
 });
+console.log(json);
+
 
 // 2. JSON to Object
 // parse(json)
+console.clear();
+json = JSON.stringify(rabbit);
+const obj = JSON.parse(json, (key, value) => {
+    console.log(`key: ${key}, value: ${value}`);
+    return key === 'birthDate' ? new Date(value) :value;
+});
+console.log(obj);
+rabbit.jump();
+// obj.jump();
+
+console.log(rabbit.birthDate.getDate());
+console.log(obj.birthDate.getDate());
