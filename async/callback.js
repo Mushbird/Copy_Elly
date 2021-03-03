1373c314cd54d0729c65371ed1cdfaaf62f936fc
mushbird <0109bsh@gmail.com>
2021-03-03 21:58:31 +0900

    [Lecture11] Intermediate Commit

---
+++ async/callback.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// JavaScript is synchronous (정해진 순서에 맞게 진행)
+// Execute the code block by orger after hoisting.
+// hoisting: var, function declaration
+console.log('1');
+setTimeout(() => console.log('2'), 1000);
+console.log('3');
+
+// Synchronous callback
+
+
+// Asynchronous callback
+
