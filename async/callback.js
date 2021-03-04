ad78702c4e713b122a1f130fcdf9f997817c48c0
mushbird <0109bsh@gmail.com>
2021-03-04 22:24:46 +0900

    [Lecture11] Intermediate Commit

--- async/callback.js
+++ async/callback.js
@@ -8,7 +8,20 @@ setTimeout(() => console.log('2'), 1000);
 console.log('3');
 
 // Synchronous callback
-
+function prontInmmediately(print) {
+    print();
+}
 
 // Asynchronous callback
+function printWithDelay(print, timeout) {
+    setTimeout(print, timeout);
+}
+// JavaScript is synchronous.
+// Execute the code block by orger after hoisting.
+// hoisting: var, function declaration
+console.log('1'); // 동기
+setTimeout(() => console.log('2'), 1000); // 비동기
+console.log('3'); // 동기
+printImmediately(() => console.log('hello')); // 동기
+printWithDelay(() => console.log('async callback'), 2000); // 비동기
 
