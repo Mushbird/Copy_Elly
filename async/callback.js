a1c92b19979897108bdfb2f096a955b31de22740
mushbird <0109bsh@gmail.com>
2021-03-05 23:22:58 +0900

    [Lecture11] Intermediate Commit

--- async/callback.js
+++ async/callback.js
@@ -25,3 +25,28 @@ console.log('3'); // 동기
 printImmediately(() => console.log('hello')); // 동기
 printWithDelay(() => console.log('async callback'), 2000); // 비동기
 
+// Callback Hell example
+class UserStorage {
+    loginUser(id, password, onSuccess, onError) {
+        setTimeout(()=> {
+            if (
+                (id === 'ellie' && password === 'dream') ||
+                (id === 'coder' && password === 'academy')
+            ) {
+                onSuccess(id);
+            } else {
+                onError(new Error('not found'));
+            }
+        }, 2000);
+    }
+
+    getRoles(user, onSuccess, onError) {
+        setTimeout(() => {
+            if (user === 'ellie') {
+                onSuccess({ name: 'ellie', role: 'admin'});
+            } else {
+                onError(new Error('no access'));
+            }
+        }, 1000);
+    }
+}
