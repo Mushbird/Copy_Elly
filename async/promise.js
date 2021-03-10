7ab45b8ec04ff29d9bf5e289382e7d8faa075ae3
mushbird <0109bsh@gmail.com>
2021-03-10 23:47:35 +0900

    [Lecture12] Intermediate Commit

--- async/promise.js
+++ async/promise.js
@@ -40,3 +40,22 @@ fetchNumber
         });
     })
     .then(num => console.log(num));
+
+// 4. Error Handling
+const getHen = () => 
+    new Promise((resolve, reject) => {
+        setTimeout(() => resolve('chick'), 1000);
+    });
+const getEgg = hen =>
+    new Promise((resolve, reject) => {
+        setTimeout(() => resolve(`${hen} => 'egg'`), 1000);
+    });
+const cook = egg =>
+    new Promise((resolve, reject) => {
+        setTimeout(() => resolve(`${egg} => 'cook'`), 1000);
+    });
+
+getHen()
+    .then(hen => getEgg(hen))
+    .then(egg => cook(egg))
+    .then(meal => console.log(meal));
