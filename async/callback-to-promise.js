a69c925362ba52803809303b4578025c5d770077
mushbird <0109bsh@gmail.com>
2021-03-13 23:30:50 +0900

    [Lecture12] Intermediate Commit

--- async/callback-to-promise.js
+++ async/callback-to-promise.js
@@ -46,9 +46,9 @@ class UserStorage {
         return new Promise((resolve, reject) => {
             setTimeout(() => {
                 if (user === 'ellie') {
-                    onSuccess({ name: 'ellie', role: 'admin'});
+                    resolve({ name: 'ellie', role: 'admin'});
                 } else {
-                    onError(new Error('no access'));
+                    reject(new Error('no access'));
                 }
             }, 1000);
         });
@@ -58,10 +58,12 @@ class UserStorage {
 const userStorage = new UserStorage();
 const id = prompt('enter your id');
 const password = prompt('enter your password');
-userStorage.loginUser(id, password)
-.then(userStorage.getRoles)
-.then(user => alert(`Hello ${user.name}, you have a ${user.role} role`))
 
+userStorage
+    .loginUser(id, password)
+    .then(userStorage.getRoles)
+    .then(user => alert('Hello ${userWithRole.name}, you have a ${userWithRole.role} role'))
+    .catch(console.log);
 
 userStorage.loginUser(
     id,
