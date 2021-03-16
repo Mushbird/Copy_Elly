5f980739b15d7733f471d1b51528f666e3296b8b
mushbird <0109bsh@gmail.com>
2021-03-16 21:56:54 +0900

    [Lecture13] Intermediate Commit

--- async/async.js
+++ async/async.js
@@ -8,3 +8,22 @@ function fetchUser() {
         resolve('ellie');
     });
 }
+
+const user = fetchUser();
+user.then(console.log);
+console.log(user);
+
+// 2. await
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function getApple() {
+    await delay(3000);
+    return 'apple';
+}
+
+async function getBanana() {
+    await delay(3000);
+    return 'banana';
+}
