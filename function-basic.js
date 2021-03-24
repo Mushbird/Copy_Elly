79b2a28e4f879b3b0db4aef0ed69d8f844f32112
mushbird <0109bsh@gmail.com>
2021-03-24 23:54:59 +0900

    [function] Intermediate Commit

---
+++ function-basic.js
@@ -0,0 +1,18 @@
+// Don`t give up
+// 포기하지 마세요
+
+// 함수 선언
+function doSomething() {
+    console.log('hello');
+}
+
+function add(a, b) {
+    const sum = a + b;
+    return sum;
+}
+
+// 함수 호출
+doSomething();
+
+const result = add(1, 2);
+console.log(result);
